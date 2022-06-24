2097b97b452914bd337be3b55719bfe555bea447
Marcos <marcos.santos@codenapp.com>
2022-06-24 16:56:51 -0300

    mudando movies mock para list movies mock

--- lib/domain/movies/models/movies.mock.dart
+++ lib/domain/movies/models/movies.mock.dart
@@ -1,19 +1,21 @@
 import 'package:ekko/domain/movies/models/movies.model.dart';
 
-const moviesMock = MoviesModel(
-  genreId: [1, 2, 3],
-  originalLanguage: 'en',
-  originalTitle: 'Back to the future',
-  voteAverage: 9,
-  releaseDate: '1985-12-25',
-  voteCount: 5154,
-  backdropPath: 'backdropPath',
-  posterPath: 'posterPath',
-  mediaType: 'movie',
-  id: 55,
-  video: false,
-  overview: 'Resumo do filme',
-  title: 'De volta para o futuro',
-  adult: false,
-  polpularity: 8000,
-);
+const listMoviesMock = [
+  MoviesModel(
+    genreId: [1, 2, 3],
+    originalLanguage: 'en',
+    originalTitle: 'Back to the future',
+    voteAverage: 9,
+    releaseDate: '1985-12-25',
+    voteCount: 5154,
+    backdropPath: 'backdropPath',
+    posterPath: 'posterPath',
+    mediaType: 'movie',
+    id: 55,
+    video: false,
+    overview: 'Resumo do filme',
+    title: 'De volta para o futuro',
+    adult: false,
+    polpularity: 8000,
+  ),
+];
