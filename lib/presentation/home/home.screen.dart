804ed7f67a367aca7308f2b5c31fa418332b0fb3
Marcos Santos <marcos.santos@codenapp.com>
2022-06-26 22:38:28 -0300

    ajustando homescreen

--- lib/presentation/home/home.screen.dart
+++ lib/presentation/home/home.screen.dart
@@ -1,4 +1,5 @@
 import 'package:ekko/domain/core/abstractions/presentation/controllers/login/home_controller.interface.dart';
+import 'package:ekko/domain/movies/models/movies.model.dart';
 import 'package:ekko/presentation/home/widgets/grid_movies.widget.dart';
 import 'package:ekko/presentation/shared/loading/loading.widget.dart';
 import 'package:ekko/presentation/shared/view_controller.interface.dart';
@@ -7,27 +8,51 @@ import 'package:flutter/material.dart';
 class HomeScreen extends ViewController<IHomeController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 17, 0, 28),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 52, 3, 67),
-        title: const Text('Filmes'),
-      ),
-      body: LoadingWidget(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              const Text(
-                'Populares',
-                style: TextStyle(color: Colors.white),
+    return StreamBuilder<List<MoviesModel>?>(
+      stream: controller.movies.stream,
+      initialData: const [],
+      builder: (_, snapshot) {
+        return StreamBuilder<List<MoviesModel>?>(
+          stream: controller.movies.stream,
+          builder: (_, snap) {
+            return Scaffold(
+              backgroundColor: const Color(0xFF19161D),
+              appBar: AppBar(
+                backgroundColor: const Color(0xFF231F2D),
+                title: const Text(
+                  'Filmes',
+                  style: TextStyle(color: Color(0xFF9182AD)),
+                ),
               ),
-              const SizedBox(height: 20),
-              Expanded(child: GridMoviesWidget()),
-            ],
-          ),
-        ),
-      ),
+              body: LoadingWidget(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Populares',
+                        style: TextStyle(color: Color(0xFF9182AD)),
+                      ),
+                      const SizedBox(height: 20),
+                      Expanded(
+                        child: GridMoviesWidget(
+                          index: snap.hasData
+                              ? snap.data!.isNotEmpty
+                                  ? snap.data!.length
+                                  : 0
+                              : 0,
+                          moviesList: snap.hasData ? snap.data! : [],
+                          onTap: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
